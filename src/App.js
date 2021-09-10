5c6eeefb389f7f7c361be8329ebbc0529f0d7323
Dimon <dmitri.pozharsky@gmail.com>
2021-09-10 12:31:42 +0300

    modified App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./component/Header";
+import Main from "./component/Main";
+import Footer from "./component/Footer"
 import './App.scss';
 
 function App() {
-  return (
-    <Header/>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
