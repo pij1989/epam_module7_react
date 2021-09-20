b93b29956f74ce0fa16362e9b213aa125b1486c6
Dimon <dmitri.pozharsky@gmail.com>
2021-09-20 17:18:14 +0300

    added changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,8 @@ import "./Login.css"
 import {login} from "../services/auth.service"
 import ErrorAuth from "./ErrorAuth";
 import {Redirect} from "react-router-dom";
+import {Formik} from "formik";
+import * as yup from "yup";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => {
@@ -50,32 +52,75 @@ export default (props) => {
         return <Redirect to="/home"/>;
     }
 
+    const validationSchema = yup.object().shape({
+        username: yup.string().max(30, 'Username field length must not be greater than 30 characters').min(3, 'Username field length must not be less than 3 characters').required(),
+        password: yup.string().required()
+    });
+
     return (
         <div className="Login">
             <div className="Login-title">Login</div>
-            <Form onSubmit={handleSubmit} className="d-flex flex-column">
-                <Form.Group className="mb-3">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Enter username" value={username}
-                                  onChange={handleChangeUsername} required/>
-                    <Form.Text className="text-muted">
-                        Username sample: 'Ivan'
-                    </Form.Text>
-                </Form.Group>
 
-                <Form.Group className="mb-3">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter password" value={password}
-                                  onChange={handleChangePassword} required/>
-                    <Form.Text className="text-muted">
-                        Password should be more then 8 and less then 40 symbol
-                    </Form.Text>
-                </Form.Group>
+            <Formik
+                validationSchema={validationSchema}
+                onSubmit={values => {
+                    console.log(values);
+                    login(values.username, values.password)
+                        .then(
+                            user => {
+                                props.handleAuthLogin(true, {username: user.username, role: user.role});
+                                console.log(props.auth)
+                            },
+                            message => {
+                                console.log(message);
+                                props.handleAuthError(true, message);
+                            });
+                }}
+                initialValues={{
+                    username: '',
+                    password: ''
+                }}
+            >
+                {({
+                      handleSubmit,
+                      handleChange,
+                      handleBlur,
+                      values,
+                      touched,
+                      isValid,
+                      errors,
+                  }) => (
+                    <Form onSubmit={handleSubmit} className="d-flex flex-column">
+                        <Form.Group className="mb-3">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text"
+                                          placeholder="Enter username"
+                                          name="username"
+                                          value={values.username}
+                                          onChange={handleChange}/>
+                            <Form.Text className="text-muted">
+                                Username sample: 'Ivan'
+                            </Form.Text>
+                        </Form.Group>
 
-                <Button variant="primary" type="submit" className="Login-button">
-                    Submit
-                </Button>
-            </Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password"
+                                          placeholder="Enter password"
+                                          name="password"
+                                          value={values.password}
+                                          onChange={handleChange}/>
+                            <Form.Text className="text-muted">
+                                Password should be more then 8 and less then 40 symbol
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit" className="Login-button">
+                            Submit
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
             {authError.isAuthError ? <ErrorAuth message={authError.errorMassage}/> : null}
         </div>
     )
